082aff5f75c536c244b5e2714747f66020f2f057
Samuel <ajalasamuel220@gmail.com>
2025-07-26 00:28:20 +0100

    universal update script.

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-word="awesome"
+release_file=/etc/os-release
 
-echo "Linux is $word"
-echo "Videogames are $word"
-echo "Sunny days are $word"
+if grep -q "Arch" $release_file
+
+then
+	# Then host is based on Arch, run the pacman update command
+	sudo pacman -Syu
+fi
+
+if grep -q "kali" $release_file || grep -q "Ubuntu" $release_file
+
+then 
+	# The host is based on Ubuntu, run the apt version of the command
+	sudo apt update
+	sudo apt dist-upgrade
+fi
